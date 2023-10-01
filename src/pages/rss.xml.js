8b35295f85f54d223caeffa6a4b78afc4408d86b
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-10-01 13:09:39 +0900

    :bug: 各記事へのリンクが切れていた箇所を修正

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from "astro:content";
 
 export async function GET() {
+  const posts = await getCollection('posts');
   return rss({
     title: 'Astroチュートリアル | ブログ',
     description: 'Astroを学ぶ旅',
     site: 'https://kyoruni.github.io/astro-tutorial/',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/astro-tutorial/posts/${post.slug}/`,
+    })),
     customData: `<language>ja-jp</language>`,
   });
 }
