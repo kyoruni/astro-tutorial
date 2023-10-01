af6c153e747706ec74554f36153479db90065760
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-10-01 12:25:29 +0900

    :wrench: Vue.jsを使ってみる

---
+++ src/components/Greeting.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  messages: {
+    type: Array,
+    required: true,
+    default: ["デフォルト"],
+  }
+});
+const greeting = ref("");
+
+const setGreeting = () => {
+  greeting.value = props.messages[(Math.floor(Math.random() * props.messages.length))];
+};
+
+const greetingButton = () => {
+  setGreeting();
+};
+
+setGreeting();
+</script>
+
+<template>
+  <div>
+    <h3>{{ greeting }}</h3>
+    <button @click="greetingButton">新しい挨拶</button>
+  </div>
+</template>
