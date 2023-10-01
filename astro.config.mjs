e388870360134c2a6c8a5fa661c538334abfeff2
kyoruni <40832190+kyoruni@users.noreply.github.com>
2023-10-01 12:03:52 +0900

    :wrench: Vue.jsを追加
    npx astro add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kyoruni.github.io',
   base: '/astro-tutorial',
+  integrations: [vue()]
 });
